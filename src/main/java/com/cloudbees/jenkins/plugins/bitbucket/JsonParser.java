2ee06c8be44863baf25c16dffa7f4224fcaeb266
Vivek Pandey <vivek.pandey@gmail.com>
2017-10-26 14:25:36 +0530

    Added license header, removed author name

--- src/main/java/com/cloudbees/jenkins/plugins/bitbucket/JsonParser.java
+++ src/main/java/com/cloudbees/jenkins/plugins/bitbucket/JsonParser.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016-2017, CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.cloudbees.jenkins.plugins.bitbucket;
 
 import com.google.common.base.Charsets;
@@ -5,8 +28,6 @@ import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.kohsuke.accmod.Restricted;
 import org.kohsuke.accmod.restrictions.NoExternalUse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,12 +39,9 @@ import java.util.TimeZone;
 
 /**
  * Jackson based JSON parser
- *
- * @author Vivek Pandey
  */
 @Restricted(NoExternalUse.class)
 public final class JsonParser {
-    private static final Logger LOGGER = LoggerFactory.getLogger(JsonParser.class);
     private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS";
     public static final ObjectMapper mapper = createObjectMapper();
 
